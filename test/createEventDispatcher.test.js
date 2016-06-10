986ce6472e5d7d2d2168eed7dcacfe037c2cd0d8
Victor Zamfir <zamfir.victor@gmail.com>
2016-06-10 15:50:39 +0300

    better priority tests

--- test/createEventDispatcher.test.js
+++ test/createEventDispatcher.test.js
@@ -242,9 +242,9 @@ describe('createEventDispatcher', () => {
   it('should be handle the subscribers using priorities', () => {
     dispatcher.subscribe('test', ({ next, params }) => next(`${params} 3`), { priority: 100 });
 
-    dispatcher.subscribe('test', ({ next, params }) => next(`${params} 5`), { priority: -1 });
+    dispatcher.subscribe('test', ({ next, params }) => next(`${params} 5`));
 
-    dispatcher.subscribe('test', ({ next, params }) => next(`${params} 4`), { priority: 10 });
+    dispatcher.subscribe('test', ({ next, params }) => next(`${params} 4`), { priority: 5 });
 
     dispatcher.subscribe('test', ({ next, params }) => next(`${params} 2`), { priority: 1000 });
 
