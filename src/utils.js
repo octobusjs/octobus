5f10efcebc15e6401b9d3ec9c6ef482923de26b5
Victor Zamfir <victor.zamfir@bengroup.com>
2016-07-27 11:57:01 +0300

    introducing middlewares

--- src/utils.js
+++ src/utils.js
@@ -34,6 +34,17 @@ export const validateEvent = (event, delimiter) => {
   return event;
 };
 
-export const createSubscribersChain = () => {
+// https://github.com/reactjs/redux/blob/master/src/compose.js
+export const compose = (...funcs) => {
+  if (funcs.length === 0) {
+    return arg => arg;
+  }
 
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  const last = funcs[funcs.length - 1];
+  const rest = funcs.slice(0, -1);
+  return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args));
 };
